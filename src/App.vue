ccf6c612bccd55136fcc51b24995f5ea522956ff
Lukasz Szmytko <lszmytko@gmail.com>
2021-12-08 15:35:39 +0100

    prettified

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,29 @@
   <div>
     <h1>Patients</h1>
     <div class="buttons_container">
-      <button @click="handleFilterAgeActive">
+      <button
+        @click="handleFilterAgeActive"
+        :class="{ active: filterAgeActive }"
+      >
         {{ filterAgeActive ? "Unfilter" : "Filter" }} age >30
       </button>
-      <button @click="handleFilterAgeAndMedicineActive">
-        {{ filterAgeAndMedicineActive ? "Unfilter" : "Filter" }} age &lt; 63
-        and medicines strength > 8
+      <button
+        @click="handleFilterAgeAndMedicineActive"
+        :class="{ active: filterAgeAndMedicineActive }"
+      >
+        {{ filterAgeAndMedicineActive ? "Unfilter" : "Filter" }} age &lt; 63 and
+        medicines strength > 8
       </button>
     </div>
-    <div class="card-container">   
-        <Card
-          v-for="patient in patientsToShow"
-          :key="patient.id"
-          :name="patient.name"
-          :lastName="patient.lastName"
-          :medicines="patient.medicines"
-          :age="patient.age"
-            />         
+    <div class="card-container">
+      <Card
+        v-for="patient in patientsToShow"
+        :key="patient.id"
+        :name="patient.name"
+        :lastName="patient.lastName"
+        :medicines="patient.medicines"
+        :age="patient.age"
+      />
     </div>
   </div>
 </template>
@@ -32,7 +38,6 @@ export default {
   name: "App",
   components: { Card },
   setup() {
-
     // Refs
     let patients = ref(null);
     let patientsAbove30 = ref(null);
@@ -45,8 +50,8 @@ export default {
     const ageCriteria = 30;
     const ageAndMedicineCriteria = {
       age: 63,
-      medicineStrength: 8
-    }
+      medicineStrength: 8,
+    };
 
     // Handling filter criteria
 
@@ -90,7 +95,6 @@ export default {
     // Patients watcher
 
     watch(patients, () => {
-
       // Generate above30 array
       patientsAbove30.value = patients.value.filter((patient) => {
         return patient.age > ageCriteria;
@@ -101,7 +105,7 @@ export default {
         return JSON.parse(JSON.stringify(item));
       });
 
-      // Generate array of below 63 and 
+      // Generate array of below 63 and
 
       patientsBelow63withMedicineStrength.value = tempArray
         .map((patientTemp) => {
@@ -115,15 +119,15 @@ export default {
         });
     });
 
-  // Generate final array to show
-   const patientsToShow = computed(function(){
-     if(filterAgeActive.value === true){
-       return patientsAbove30.value;
-     } else if(filterAgeAndMedicineActive.value === true){
-       return patientsBelow63withMedicineStrength.value;
-     }
-     return patients.value;
-   })
+    // Generate final array to show
+    const patientsToShow = computed(function () {
+      if (filterAgeActive.value === true) {
+        return patientsAbove30.value;
+      } else if (filterAgeAndMedicineActive.value === true) {
+        return patientsBelow63withMedicineStrength.value;
+      }
+      return patients.value;
+    });
 
     return {
       patients,
@@ -178,11 +182,11 @@ ul {
 button {
   background-color: rgb(142, 202, 230);
   border: 1px solid transparent;
-  border-radius: .5rem;
+  border-radius: 0.5rem;
   box-sizing: border-box;
   cursor: pointer;
   display: inline-block;
-  font-size: .75rem;
+  font-size: 0.75rem;
   font-weight: 400;
   line-height: 1.15385;
   margin: 0;
@@ -193,19 +197,14 @@ button {
   letter-spacing: 1.5px;
 }
 
-button:hover,
-button:focus {
+.active {
   background-color: #219ebc;
-}
-
-button:focus {
   box-shadow: 0 0 0 4px rgba(0, 149, 255, 0.15);
 }
 
-@media screen and (min-width: 768px){
-  button{
+@media screen and (min-width: 768px) {
+  button {
     font-size: 1rem;
   }
 }
-
 </style>
