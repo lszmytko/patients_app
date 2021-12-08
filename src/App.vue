264e48f877b30d29d5e141a3fd39402bd9bf1307
Lukasz Szmytko <lszmytko@gmail.com>
2021-12-08 14:33:08 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,192 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Patients</h1>
+    <div class="buttons_container">
+      <button @click="handleFilterAgeActive">
+        {{ filterAgeActive ? "Unfilter" : "Filter" }} age >30
+      </button>
+      <button @click="handleFilterAgeAndMedicineActive">
+        {{ filterAgeAndMedicineActive ? "Unfilter" : "Filter" }} age &lt; 63
+        and medicines strength > 8
+      </button>
+    </div>
+    <div class="card-container">   
+        <Card
+          v-for="patient in patientsToShow"
+          :key="patient.id"
+          :name="patient.name"
+          :lastName="patient.lastName"
+          :medicines="patient.medicines"
+          :age="patient.age"
+            />         
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { watch, ref, computed } from "vue";
+import axios from "axios";
+import Card from "./components/Card.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Card },
+  setup() {
+    let patients = ref(null);
+    let patientsAbove30 = ref(null);
+    let patientsBelow63withMedicineStrength = ref(null);
+    let filterAgeActive = ref(false);
+    let filterAgeAndMedicineActive = ref(false);
+
+    function handleFilterAgeActive() {
+      this.filterAgeActive = !this.filterAgeActive;
+      if (this.filterAgeAndMedicineActive) {
+        this.filterAgeAndMedicineActive = false;
+      }
+    }
+
+    function handleFilterAgeAndMedicineActive() {
+      this.filterAgeAndMedicineActive = !this.filterAgeAndMedicineActive;
+      if (this.filterAgeActive) {
+        this.filterAgeActive = false;
+      }
+    }
+
+    // Initial fetch
+
+    axios
+      .all([
+        axios.get("https://cerber.pixel.com.pl/api/patients"),
+        axios.get("https://cerber.pixel.com.pl/api/medicine"),
+      ])
+      .then(
+        axios.spread(function (response1, response2) {
+          const patientsFetched = response1.data.map((patient) => {
+            patient.medicines = [];
+            for (const medicine of response2.data) {
+              if (medicine.patientIds.includes(patient.id)) {
+                patient["medicines"].push(medicine);
+              }
+            }
+            return patient;
+          });
+          patients.value = patientsFetched;
+        })
+      )
+      .catch((e) => console.log(e));
+
+    watch(patients, () => {
+      patientsAbove30.value = patients.value.filter((patient) => {
+        return patient.age > 30;
+      });
+
+      const tempArray = [...patients.value].map((item) => {
+        return JSON.parse(JSON.stringify(item));
+      });
+
+      patientsBelow63withMedicineStrength.value = tempArray
+        .map((patientTemp) => {
+          patientTemp.medicines = patientTemp.medicines.filter((medicine) => {
+            return medicine.strength > 8;
+          });
+          return patientTemp;
+        })
+        .filter((patientTemp) => {
+          return patientTemp.age < 63;
+        });
+    });
+
+   const patientsToShow = computed(function(){
+     if(filterAgeActive.value === true){
+       return patientsAbove30.value;
+     } else if(filterAgeAndMedicineActive.value === true){
+       return patientsBelow63withMedicineStrength.value;
+     }
+     return patients.value;
+   })
+
+    return {
+      patients,
+      patientsToShow,
+      filterAgeActive,
+      filterAgeAndMedicineActive,
+      handleFilterAgeActive,
+      handleFilterAgeAndMedicineActive,
+      patientsAbove30,
+      patientsBelow63withMedicineStrength,
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Libre Baskerville", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
+
+h1 {
+  color: #2b2d42;
+  padding: 1.5rem;
+}
+
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  justify-content: center;
+  width: 100%;
+  gap: 2rem;
+}
+
+.buttons_container {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-bottom: 2rem;
+  padding: 0 2rem;
+}
+
+ul {
+  padding: 0;
+}
+
+button {
+  background-color: rgb(142, 202, 230);
+  border: 1px solid transparent;
+  border-radius: .5rem;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  font-size: .75rem;
+  font-weight: 400;
+  line-height: 1.15385;
+  margin: 0;
+  outline: none;
+  padding: 8px 0.8em;
+  text-align: center;
+  text-decoration: none;
+  letter-spacing: 1.5px;
+}
+
+button:hover,
+button:focus {
+  background-color: #219ebc;
+}
+
+button:focus {
+  box-shadow: 0 0 0 4px rgba(0, 149, 255, 0.15);
+}
+
+@media screen and (min-width: 768px){
+  button{
+    font-size: 1rem;
+  }
+}
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="Card">
+    <h3>{{ name }} {{ lastName }}</h3>
+    <h4>Age: {{ age }}</h4>
+    <h5>{{medicines.length ? 'Medicine list' : 'No medicines'}}</h5>
+    <ul v-if="medicines.length">
+      <li v-for="medicine in medicines" :key="medicine.id">
+        {{ medicine.medicationName }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["name", "lastName", "medicines", "age"],
+};
+</script>
+
+<style scoped>
+.Card {
+  width: 300px;
+  background: rgb(142, 202, 230);
+  border-radius: 1rem;
+  padding: 1rem;
+}
+
+h3 {
+  letter-spacing: 0.1.5rem;
+  font-weight: 700;
+  margin-bottom: .75rem;
+  text-align: center;
+}
+
+h4 {
+  margin-bottom: 1rem;
+}
+
+h5{
+  margin-bottom: .5rem;
+}
+
+
+ul{
+  list-style: none;
+}
+
+li {
+  font-size: 0.75rem;
+  margin-bottom: 0.25rem;
+}
+</style>
